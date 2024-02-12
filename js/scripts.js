b9dc714871e6509daf48a0b4e372a20e14441fbf
notavailable4u <139932183+notavailable4u@users.noreply.github.com>
2024-02-12 11:08:26 -0600

    Create pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-alert('Hello World');
-
-let favoriteFood = 'French Silk Pie';
-document.write(favoriteFood);
+const pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 2.04,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmeleon',
+        height: 3.07,
+        types: [fire],
+    },
+    {
+        name: Ivysaur,
+        height: 3.03,
+        types: [grass, poison]
+    }
+];
